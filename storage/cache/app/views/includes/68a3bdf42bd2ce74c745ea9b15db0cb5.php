26af159db4e4b2989b2dac90a1aa532daebfad27
jeandev84 <jeandev84@yahoo.com>
2022-04-10 01:14:15 +0300

    remove views cache

--- storage/cache/app/views/includes/68a3bdf42bd2ce74c745ea9b15db0cb5.php
+++
@@ -1,6 +0,0 @@
-<article>
-    <h2>Блог</h2>
-    <div class="blog__photo_gallery text__pretty">
-        <p>Пишу о моде, гардеробе, создании своего уникального стиля и развитии вкуса</p>
-    </div>
-</article>
