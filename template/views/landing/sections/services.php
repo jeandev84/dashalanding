55fc2748bf466e2c1f1912c7f9711fda8fd534b5
jeandev84 <jeandev84@yahoo.com>
2022-04-10 02:35:05 +0300

    accordion services

--- template/views/landing/sections/services.php
+++ template/views/landing/sections/services.php
@@ -1,3 +1,56 @@
 <article>
      <h2>Описание услуг и цены</h2>
+     <div class="service">
+         <div class="service-item">
+             <input type="checkbox" id="service__one" class="service-input">
+             <label for="service__one" class="service-title bg_red_pretty">Авторская техника энергомассажа.</label>
+             <div class="service-text text__pretty">
+                 <p>
+                     Почему авторская?  По тому что была придумана и разработана лично мной.
+                     На энергомассаже мы прорабатываем энерготела на чистку от негатива,
+                     на заживление ран и ссадин в них, на обновление энергии.
+                     Что это дает: это глоток свежего воздуха для энергетических тел и в последствии - для физического тела.
+                     Это дополнительные силы для борьбы с чем угодно. Это помощь в избавлении от депрессии.
+                     Это дополнительная помощь при суицидальных мыслях, так как при них возможен сильный наведенный или самонаведенный негатив.
+                 </p>
+                 <span class="service__price">*2 500 р</span>
+             </div>
+         </div>
+         <div class="service-item">
+             <input type="checkbox" id="service__two" class="service-input">
+             <label for="service__two" class="service-title bg_red_pretty">Авторская техника "перезапуск энергоцентров".</label>
+             <div class="service-text text__pretty">
+                 <p>
+                     Мы пробуждаем спящие энергоцентры (чакры), сдуваем с них пыль, чистим от негатива и застоя энергии, убираем блоки.
+                     Перезапускаем энергию и как следствие получаем качественно лучшее самочувствие,
+                     энергообмен со вселенной, решаются вопросы, которые были отложены в дальний ящик,
+                     лучше соображается, приходят новые идеи и озарения. Качественно улучшаетя жизнь.
+                 </p>
+                 <span class="service__price">*1900 р.</span>
+             </div>
+         </div>
+         <div class="service-item">
+             <input type="checkbox" id="service__three" class="service-input">
+             <label for="service__three" class="service-title bg_red_pretty">Энергочистка на расстоянии / по фото</label>
+             <div class="service-text text__pretty">
+                 <p>
+                     Прорабатываем энерготела на чистку от негатива, на заживление ран и ссадин в них, на обновление энергии.
+                     Что это дает: это глоток свежего воздуха для энергетических тел и в последствии - для физического тела.
+                     Это дополнительные силы для борьбы с чем угодно. Помощь в избавлении от депрессии.
+                     Помощь при суицидальных мыслях, так как при них возможен сильный наведенный или самонаведенный негатив
+                 </p>
+                 <span class="service__price">*2000 р.</span>
+             </div>
+         </div>
+         <div class="service-item">
+             <input type="checkbox" id="service__four" class="service-input">
+             <label for="service__four" class="service-title bg_red_pretty">Энергодиагностика общего состояния</label>
+             <div class="service-text text__pretty">
+                 <p>
+                     Диагностика вашей энергетики на расстоянии / по фото.
+                 </p>
+                 <span class="service__price">*800 р.</span>
+             </div>
+         </div>
+     </div>
 </article>
